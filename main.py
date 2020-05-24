a5ca54d4341093e35748225188d707f601740599
rami <Rami.amro.ahmed@gmail.com>
2020-05-24 17:43:16 +0200

    Kaggle Submittable Code
    Implement Model selection using highest score, save the generated output to data folder. this implementation achieves 0.78947 accuracy on kaggle

--- main.py
+++ main.py
@@ -1,71 +1,111 @@
-import matplotlib.pyplot as plt
+# Importing the libraries
+
+import numpy as np
 import pandas as pd
+import seaborn as sns
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
-from sklearn.ensemble import VotingClassifier
 from sklearn.linear_model import LogisticRegression, SGDClassifier
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 
-from Handlers.LoadData import LoadData
+sns.set()
 
-data_set = LoadData().data_set
 
-x = data_set[['Pclass', 'SibSp', 'Parch', 'Fare', 'processed_age', 'labeled_Sex', 'labeled_cabin']]
-y = data_set['Survived']
+def preprocess(df):
+    # Drop un-useful columns
+    df = df.drop(['Name', 'Ticket', 'Cabin'], axis=1)
+    df["Embarked"] = df["Embarked"].interpolate()
+    df["Age"] = df["Age"].interpolate()
+    df["Fare"] = df["Fare"].interpolate()
+    # Select All columns for X except the Survived
+    X = df.loc[:, df.columns != 'Survived']
+    # X = X.loc[:, X.columns != 'PassengerId']
 
-for feature in ["Pclass","SibSp", 'Parch', 'Fare', 'processed_age', 'labeled_Sex', 'labeled_cabin']:
-    plt.scatter(data_set[feature], data_set['Survived'])
-    plt.xlabel(feature)
-    plt.ylabel("Survived")
-    plt.show()
+    # Encoding categorical data
+    dummies = []
+    cols = ['Pclass', 'Sex', 'Embarked']
+    for col in cols:
+        dummies.append(pd.get_dummies(X[col], drop_first=True))
 
-X_train, X_test, y_train, y_test = train_test_split(x.values, y.values, test_size=0.3, random_state=0)
+    X = pd.concat((X,
+                   pd.concat(dummies, axis=1)
+                   ), axis=1)
+    X = X.drop(['Pclass', 'Sex', 'Embarked'], axis=1)
+
+    # # Taking care of missing data
+    # imputer = SimpleImputer(missing_values=np.nan, strategy='median')
+    # imputer.fit(X[["Age", "Fare"]])
+    # X[["Age", "Fare"]] = imputer.transform(X[["Age", "Fare"]])
+
+    # Feature Scaling
+    # sc = StandardScaler()
+    # X = sc.fit_transform(X)
+
+    return X
+
+
+# Importing the dataset
+
+dataset = pd.read_csv("./data/train.csv")
+dataset_test = pd.read_csv("./data/test.csv")
+
+y = dataset["Survived"].values
+
+X_train = preprocess(dataset)
+X_train = X_train.loc[:, X_train.columns != 'PassengerId']
+X_results = preprocess(dataset_test)
+
+# Splitting the dataset into the Training set and Test set
+X_train_train, X_train_test, y_train_train, y_train_test = train_test_split(X_train, y, test_size=0.3, random_state=0)
 
 names = ["K Nearest Neighbors", "Decision Tree", "Random Forest", "Logistic Regression", "SGD Classifier",
-         "Naive Bayes", "SVM Linear","GB"]
+         "Naive Bayes", "SVM Linear", "GB"]
 
 classifiers = [
     KNeighborsClassifier(),
-    DecisionTreeClassifier(),
-    RandomForestClassifier(n_estimators=100),
-    LogisticRegression(max_iter=1000),
-    SGDClassifier(max_iter=100),
+    DecisionTreeClassifier(random_state=0),
+    RandomForestClassifier(n_estimators=50, random_state=0),
+    LogisticRegression(max_iter=1000, random_state=0),
+    SGDClassifier(max_iter=100, random_state=0),
     MultinomialNB(),
-    SVC(kernel='linear'),
-    GradientBoostingClassifier(n_estimators=50)
+    SVC(random_state=0),
+    GradientBoostingClassifier(n_estimators=100, random_state=0)
 ]
 
 models = list(zip(names, classifiers))
 predictions_df = pd.DataFrame()
-predictions_df["Actual"] = y_test
+predictions_df["Actual"] = y_train_test
 
-# Try out Several algorithms independently looking for the most accurate model predictions
+most_accurate_model = None
+max_accuracy = 0
 for name, model in models:
     print("==============={}=================".format(name))
     # Fit the dataset to the current algorithm
-    model.fit(X_train, y_train)
+    model.fit(X_train_train, y_train_train)
 
-    prediction = model.predict(X_test)
+    prediction = model.predict(X_train_test)
     # append each prediction from the current algorithm to a dataframe for comparision
     predictions_df["{} Predictions".format(name)] = prediction
 
+    score = model.score(X_train_test, y_train_test)
     # Generate the accuracy score for the current model
-    print("{} Score: {}".format(name, model.score(X_test, y_test)))
+    print("{} Score: {}".format(name, model.score(X_train_test, y_train_test)))
 
-    print(classification_report(y_test, prediction))
-    print(confusion_matrix(y_test, prediction))
+    if score > max_accuracy:
+        max_accuracy = score
+        most_accurate_model = model
 
-print("===============Voting Classifier=================")
-votingClassifier = VotingClassifier(estimators=models, voting='hard', n_jobs=-1)
-votingClassifier.fit(X_train, y_train)
+    # print(classification_report(y_test, prediction))
+    print(confusion_matrix(y_train_test, prediction))
+    #     todo evaluate model based on CAP method instead of accuracy
 
-print("Voting Classifier: Score: {}".format(votingClassifier.score(X_test, y_test)))
-predictions_df["Voting Classifier Predictions"] = votingClassifier.predict(X_test)
-prediction = votingClassifier.predict(X_test)
-
-print(classification_report(y_test, prediction))
-print(confusion_matrix(y_test, prediction))
+print(most_accurate_model)
+clf = most_accurate_model
+y_results = clf.predict(X_results.iloc[:, 1:].values)
+output = np.column_stack((X_results.iloc[:, 0].values, y_results))
+df_results = pd.DataFrame(output.astype('int'), columns=['PassengerID', 'Survived'])
+df_results.to_csv('./data/titanic_results.csv', index=False)
