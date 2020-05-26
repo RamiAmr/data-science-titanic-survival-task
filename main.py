e980d87dcb8d0cf8db0840c33e4f1f73d008c4ed
rami <Rami.amro.ahmed@gmail.com>
2020-05-26 18:19:55 +0200

    Kaggle Submittable Code (ANN)
    first attempt to predict results using an Artificial neural network 0.77990 accuracy on kaggle

--- main.py
+++ main.py
@@ -1,111 +1,138 @@
 # Importing the libraries
-
 import numpy as np
 import pandas as pd
 import seaborn as sns
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
-from sklearn.linear_model import LogisticRegression, SGDClassifier
-from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
-from sklearn.tree import DecisionTreeClassifier
+import tensorflow as tf
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler
+
+print(tf.__version__)
 
 sns.set()
 
 
+def init_ann(X, y):
+    # init the ANN
+    ann = tf.keras.models.Sequential()
+    # create the first hidden layer with 6 nodes
+    ann.add(tf.keras.layers.Dense(units=6, activation="relu"))
+    # create another hidden layer with 6 nodes
+    ann.add(tf.keras.layers.Dense(units=6, activation="relu"))
+    # create the output layer
+    ann.add(tf.keras.layers.Dense(units=1, activation="sigmoid"))
+    # Compile the ANN
+    ann.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+    # Train the ANN
+    ann.fit(X, y, batch_size=32, epochs=200)
+
+    return ann
+
+
 def preprocess(df):
-    # Drop un-useful columns
-    df = df.drop(['Name', 'Ticket', 'Cabin'], axis=1)
-    df["Embarked"] = df["Embarked"].interpolate()
-    df["Age"] = df["Age"].interpolate()
-    df["Fare"] = df["Fare"].interpolate()
-    # Select All columns for X except the Survived
-    X = df.loc[:, df.columns != 'Survived']
-    # X = X.loc[:, X.columns != 'PassengerId']
+    global cabin_imputer, numeric_imputer, embarked_imputer, sex_le, ct, sc
 
-    # Encoding categorical data
-    dummies = []
-    cols = ['Pclass', 'Sex', 'Embarked']
-    for col in cols:
-        dummies.append(pd.get_dummies(X[col], drop_first=True))
+    num_vars = ["Age", "Fare"]
+    numeric_imputer.fit(df[num_vars])
+    df[num_vars] = numeric_imputer.transform(df[num_vars])
+    cabin_imputer.fit(df[["Cabin"]])
+    df[["Cabin"]] = cabin_imputer.transform(df[["Cabin"]])
+    # Remove all cabin numbers and leave out only the cabin letters indicating what level on the titanic was that person
+    df["Cabin"] = df["Cabin"].apply(lambda x: ''.join(i.lower() for i in x if not i.isdigit()))
+    # handel several cabin letters that where generated ex. B58 F60 -> b f -> b
+    df["Cabin"] = df["Cabin"].apply(lambda x: x.strip()[0])
+    embarked_imputer.fit(df[["Embarked"]])
+    df[["Embarked"]] = embarked_imputer.transform(df[["Embarked"]])
+    df["Embarked"] = df["Embarked"].str.lower()
+    # encode Gender Column using label encoder
+    sex_le.fit(df["Sex"])
+    df["Sex"] = sex_le.transform(df["Sex"])
+    # encode Embarked column using OneHotEncoder
+    ct.fit(df)
+    df = ct.transform(df)
+    # Feature Scaling (All inputs to an ANN Must Be Scaled down)
+    sc.fit(df)
+    df = sc.transform(df)
 
-    X = pd.concat((X,
-                   pd.concat(dummies, axis=1)
-                   ), axis=1)
-    X = X.drop(['Pclass', 'Sex', 'Embarked'], axis=1)
+    return df
 
-    # # Taking care of missing data
-    # imputer = SimpleImputer(missing_values=np.nan, strategy='median')
-    # imputer.fit(X[["Age", "Fare"]])
-    # X[["Age", "Fare"]] = imputer.transform(X[["Age", "Fare"]])
 
-    # Feature Scaling
-    # sc = StandardScaler()
-    # X = sc.fit_transform(X)
+def preprocess_test(df):
+    global cabin_imputer, numeric_imputer, embarked_imputer, sex_le, ct, sc
 
-    return X
+    num_vars = ["Age", "Fare"]
+
+    df[num_vars] = numeric_imputer.transform(df[num_vars])
+
+    df[["Cabin"]] = cabin_imputer.transform(df[["Cabin"]])
+    # Remove all cabin numbers and leave out only the cabin letters indicating what level on the titanic was that person
+    df["Cabin"] = df["Cabin"].apply(lambda x: ''.join(i.lower() for i in x if not i.isdigit()))
+    # handel several cabin letters that where generated ex. B58 F60 -> b f -> b
+    df["Cabin"] = df["Cabin"].apply(lambda x: x.strip()[0])
+
+    df[["Embarked"]] = embarked_imputer.transform(df[["Embarked"]])
+    df["Embarked"] = df["Embarked"].str.lower()
+    # encode Gender Column using label encoder
+    df["Sex"] = sex_le.transform(df["Sex"])
+    # encode Embarked column using OneHotEncoder
+    df = ct.transform(df)
+    # Feature Scaling (All inputs to an ANN Must Be Scaled down)
+    df = sc.transform(df)
+
+    return df
 
 
 # Importing the dataset
-
 dataset = pd.read_csv("./data/train.csv")
-dataset_test = pd.read_csv("./data/test.csv")
 
 y = dataset["Survived"].values
 
-X_train = preprocess(dataset)
-X_train = X_train.loc[:, X_train.columns != 'PassengerId']
-X_results = preprocess(dataset_test)
+# Drop un-useful columns
+X = dataset.drop(['PassengerId', "Survived", 'Name', 'Ticket'], axis=1)
+
+# fill missing values
+# TODO figure out the optimal way to fill the missing values
+
+embarked_imputer = SimpleImputer(missing_values=np.nan, strategy='most_frequent')
+cabin_imputer = SimpleImputer(missing_values=np.nan, strategy='constant', fill_value="na")
+numeric_imputer = SimpleImputer(missing_values=np.nan, strategy='median')
+ct = ColumnTransformer(transformers=[
+    # ('ss', StandardScaler(), ['max_temp',
+    #                           'avg_temp',
+    #                           'min_temp']),
+    ('encoder', OneHotEncoder(), ["Embarked", "Cabin"])
+], remainder='passthrough')
+sex_le = LabelEncoder()
+sc = StandardScaler()
+
+X = preprocess(X)
 
 # Splitting the dataset into the Training set and Test set
-X_train_train, X_train_test, y_train_train, y_train_test = train_test_split(X_train, y, test_size=0.3, random_state=0)
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-names = ["K Nearest Neighbors", "Decision Tree", "Random Forest", "Logistic Regression", "SGD Classifier",
-         "Naive Bayes", "SVM Linear", "GB"]
+model = init_ann(X, y)
 
-classifiers = [
-    KNeighborsClassifier(),
-    DecisionTreeClassifier(random_state=0),
-    RandomForestClassifier(n_estimators=50, random_state=0),
-    LogisticRegression(max_iter=1000, random_state=0),
-    SGDClassifier(max_iter=100, random_state=0),
-    MultinomialNB(),
-    SVC(random_state=0),
-    GradientBoostingClassifier(n_estimators=100, random_state=0)
-]
+# y_pred = model.predict(X)
+# y_pred = (y_pred > 0.5)
+# print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
 
-models = list(zip(names, classifiers))
-predictions_df = pd.DataFrame()
-predictions_df["Actual"] = y_train_test
+# Making the Confusion Matrix
+# from sklearn.metrics import confusion_matrix, accuracy_score
+#
+# cm = confusion_matrix(y_test, y_pred)
+# print(cm)
+# print(accuracy_score(y_test, y_pred))
 
-most_accurate_model = None
-max_accuracy = 0
-for name, model in models:
-    print("==============={}=================".format(name))
-    # Fit the dataset to the current algorithm
-    model.fit(X_train_train, y_train_train)
+dataset_test = pd.read_csv("./data/test.csv")
 
-    prediction = model.predict(X_train_test)
-    # append each prediction from the current algorithm to a dataframe for comparision
-    predictions_df["{} Predictions".format(name)] = prediction
+X_results = preprocess_test(
+    dataset_test.drop(['PassengerId', 'Name', 'Ticket'], axis=1).copy()
+)
 
-    score = model.score(X_train_test, y_train_test)
-    # Generate the accuracy score for the current model
-    print("{} Score: {}".format(name, model.score(X_train_test, y_train_test)))
-
-    if score > max_accuracy:
-        max_accuracy = score
-        most_accurate_model = model
-
-    # print(classification_report(y_test, prediction))
-    print(confusion_matrix(y_train_test, prediction))
-    #     todo evaluate model based on CAP method instead of accuracy
-
-print(most_accurate_model)
-clf = most_accurate_model
-y_results = clf.predict(X_results.iloc[:, 1:].values)
-output = np.column_stack((X_results.iloc[:, 0].values, y_results))
+y_results = model.predict(X_results)
+y_results = (y_results > 0.5)
+# y_results = model.predict(X_results.iloc[:, 1:].values)
+output = np.column_stack((dataset_test.iloc[:, 0].values, y_results))
 df_results = pd.DataFrame(output.astype('int'), columns=['PassengerID', 'Survived'])
 df_results.to_csv('./data/titanic_results.csv', index=False)
+
